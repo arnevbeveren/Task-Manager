8a77df559978044ba105549601f644b665e5818a
arnevbeveren <arne.vanbeveren@gmail.com>
2017-08-09 11:00:22 +0200

    basic front end register page

--- login.php
+++ login.php
@@ -1,10 +1,59 @@
 <!doctype html>
 <html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="css/register.css">
+<meta charset="UTF-8">
 <head>
     <meta charset="UTF-8">
     <title>Task Manager</title>
 </head>
 <body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Task Manager</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="login.php">Login</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+
+<div class="col-md-4 col-md-offset-4">
+    <h2>Register.</h2>
+    <div class="input-group">
+
+        <input id="textvak" type="text" class="form-control" placeholder="First name" name="firstname" aria-describedby="basic-addon1">
+
+
+
+        <input id="textvak" type="text" class="form-control" placeholder="Last name" name="lastname" aria-describedby="basic-addon1">
+
+
+
+        <input id="textvak" type="text" class="form-control" placeholder="Email" name="email" aria-describedby="basic-addon1">
+
+
+
+        <input id="textvak" type="text" class="form-control" placeholder="Password" name="password" aria-describedby="basic-addon1">
+
+</div>
 
 </body>
 </html>
