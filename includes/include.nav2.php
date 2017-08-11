822d3ca9eba55a9cf9b5970ed6926ac8d8283431
arnevbeveren <arne.vanbeveren@gmail.com>
2017-08-11 14:06:45 +0200

    logout

--- includes/include.nav2.php
+++ includes/include.nav2.php
@@ -15,7 +15,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="login.php">Logout</a></li>
+                <li><a href="logout.php">Logout</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
